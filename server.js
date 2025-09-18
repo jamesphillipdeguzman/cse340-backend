0875f9eb4b127f2435d8e4e8aa7ef50e3056538e
jamesphillipdeguzman <jamesphillipdeguzman@gmail.com>
2025-09-18 20:39:27 +0800

    W03 submission for Content Delivery and the MVC Pattern

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController.js");
 const inventoryRoute = require("./routes/inventoryRoute.js");
+const utilities = require("./utilities/");
 
 /* ***********************
  * View Engine and Templates
@@ -32,8 +33,35 @@ app.use(static);
 // app.get("/", (req, res) => {
 //   res.render("index", { title: "Home" });
 // });
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome));
 app.use("/inv", inventoryRoute);
+
+// File not found route
+app.use(async (req, res, next) => {
+  next({
+    status: 404,
+    message: `<span class="errormsg">Sorry, we couldn't find that page!</span>`,
+  });
+});
+
+/* ***********************
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) {
+    message = err.message;
+  } else {
+    message = `<span class="errormsg">Oh no! There was a crash. Maybe try a different route?</span>`;
+  }
+  res.render("errors/error", {
+    title: err.status || "Server Error",
+    message,
+    nav,
+  });
+});
 
 /* ***********************
  * Local Server Information
